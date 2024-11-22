731bd2d57456861a378641a9018ecb71422ac50c
TechUsopp <kevin1080p.mdm@gmail.com>
2024-11-21 22:00:37 -0300

    Adicionando as telas para o App

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import react from "react";
+import { View, Text, Button } from "react-native";
+
+export default function HomeScreen({navigation}){
+    return(
+        <View style={{
+            flex: 1, 
+            AlignItens: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text>Home Screen</Text>
+            <Button
+                title='Ir para Detalhes'
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
+    )
+}
