1910c57ee15b134d50aeed29bcaad402ac706be9
TechUsopp <kevin1080p.mdm@gmail.com>
2024-11-27 22:22:48 -0300

    Adicionando a parte de navegação TabBAr com icones

--- App-Stack.js
+++ App-Stack.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
-import Icon from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,8 +11,8 @@ export default function App(){
   return(
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="home" color={color} size={size}/>)}}/>
-        <Tab.Screen name="Details" component={DetailsScreen} options={{tabBarIcon: ({color, size}) => (<Icon name="grid" color={color} size={size}/>)}}/>
+        <Tab.Screen name="Home" component={HomeScreen}  />
+        <Tab.Screen name="Details" component={DetailsScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
   )
